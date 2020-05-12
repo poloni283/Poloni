14be105fe77f9b6b883a731a9b516be59a2c071c
poloni <poloni@etud.insa-toulouse.fr>
2020-05-12 10:35:34 +0200

    version 20200512 : Mon premiercommit

--- compile_linux.sh
+++ compile_linux.sh
@@ -1,3 +1,4 @@
  #!/usr/bin/bash
+cd src
 echo "g++ -Wall -std=c++11 -lpthread board.cpp core_simulation.cpp mydevices.cpp sketch_ino.cpp -o arduino"
 g++ -Wall -std=c++11 -pthread board.cpp core_simulation.cpp mydevices.cpp sketch_ino.cpp -o arduino
